0014f21d2f1b6d53d002e0014750d0bf7e77dee4
dcj152070 <111758889+dcj152070@users.noreply.github.com>
2022-10-14 17:07:14 +0200

    Update GPSUtils.java

--- src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
+++ src/no/hvl/dat100ptc/oppgave3/GPSUtils.java
@@ -1,52 +1,58 @@
 package no.hvl.dat100ptc.oppgave3;
 
 import static java.lang.Math.*;
-
-import no.hvl.dat100ptc.TODO;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import no.hvl.dat100ptc.oppgave1.GPSPoint;
 
 public class GPSUtils {
 
 	public static double findMax(double[] da) {
 
-		double max; 
-		
+		double max;
+
 		max = da[0];
-		
+
 		for (double d : da) {
 			if (d > max) {
 				max = d;
 			}
 		}
-		
+
 		return max;
 	}
 
 	public static double findMin(double[] da) {
+
 		// TODO - START
+
 		double min;
-		
+
 		min = da[0];
-		
-		for(double d : da) {
-			if(d < min) {
+
+		for (double d : da) {
+			if (d < min) {
 				min = d;
 			}
 		}
 		return min;
 
-
-		// TODO - SLUT
+		// TODO - SLUTT
 
 	}
 
 	public static double[] getLatitudes(GPSPoint[] gpspoints) {
 
 		// TODO - START
-		
-				
-		throw new UnsupportedOperationException(TODO.method());
-		
+
+		double[] latitudes = new double[gpspoints.length];
+
+		for (int i = 0; i < gpspoints.length; i++) {
+
+			latitudes[i] = gpspoints[i].getLatitude();
+		}
+		return latitudes;
+
 		// TODO - SLUTT
 	}
 
@@ -54,25 +60,46 @@ public class GPSUtils {
 
 		// TODO - START
 
-		throw new UnsupportedOperationException(TODO.method());
-		
+		double[] longitudes = new double[gpspoints.length];
+
+		for (int i = 0; i < gpspoints.length; i++) {
+
+			longitudes[i] = gpspoints[i].getLongitude();
+		}
+		return longitudes;
+
 		// TODO - SLUTT
 
 	}
 
-	private static int R = 6371000; // jordens radius
+	private static int R = 6371000; // jordens radius i meter
 
 	public static double distance(GPSPoint gpspoint1, GPSPoint gpspoint2) {
 
-		double d;
-		double latitude1, longitude1, latitude2, longitude2;
-
 		// TODO - START
 
-		throw new UnsupportedOperationException(TODO.method());
+		double d, a, c;
+		double latitude1, longitude1, latitude2, longitude2;
+
+		latitude1 = toRadians(gpspoint1.getLatitude());
+		longitude1 = toRadians(gpspoint1.getLongitude());
+
+		latitude2 = toRadians(gpspoint2.getLatitude());
+		longitude2 = toRadians(gpspoint2.getLongitude());
+
+		double latD = latitude2 - latitude1;
+		double lonD = longitude2 - longitude1;
+
+		a = Math.pow(Math.sin(latD / 2), 2)
+				+ Math.cos(latitude1) * Math.cos(latitude2) * Math.pow(Math.sin(lonD / 2), 2);
+
+		c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+		d = R * c;
+
+		return d;
 
 		// TODO - SLUTT
-
 	}
 
 	public static double speed(GPSPoint gpspoint1, GPSPoint gpspoint2) {
@@ -82,7 +109,12 @@ public class GPSUtils {
 
 		// TODO - START
 
-		throw new UnsupportedOperationException(TODO.method());
+		secs = gpspoint2.getTime() - gpspoint1.getTime();
+
+		double distance = GPSUtils.distance(gpspoint2, gpspoint1);
+		speed = (distance / secs) * 3.6;
+
+		return speed;
 
 		// TODO - SLUTT
 
@@ -95,12 +127,46 @@ public class GPSUtils {
 
 		// TODO - START
 
-		throw new UnsupportedOperationException(TODO.method());
-		
+		int timer, minutter, sekunder;
+
+		timer = secs / 3600;
+		minutter = (secs % 3600) / 60;
+		sekunder = (secs % 3600) % 60;
+
+		timestr = formatTimeNumber(timer) + TIMESEP + formatTimeNumber(minutter) + TIMESEP + formatTimeNumber(sekunder);
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < TEXTWIDTH; i++) {
+			sb.append(' ');
+		}
+
+		return sb.substring(timestr.length()) + timestr;
+
 		// TODO - SLUTT
 
 	}
+
 	private static int TEXTWIDTH = 10;
+
+	// Gjør tid om til tekst
+
+	public static String formatTimeNumber(int n) {
+
+		String FormatTime;
+
+		if (n < 10) {
+
+			FormatTime = "0" + n;
+			return FormatTime;
+
+		} else {
+
+			FormatTime = Integer.toString(n);
+			return FormatTime;
+
+		}
+	}
 
 	public static String formatDouble(double d) {
 
@@ -108,9 +174,20 @@ public class GPSUtils {
 
 		// TODO - START
 
-		throw new UnsupportedOperationException(TODO.method());
+		DecimalFormat df = new DecimalFormat("#.##");
+
+		df.setRoundingMode(RoundingMode.CEILING);
+		str = df.format(d);
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < TEXTWIDTH; i++) {
+			sb.append(' ');
+		}
+
+		return sb.substring(str.length()) + str;
 
 		// TODO - SLUTT
-		
 	}
+
 }
