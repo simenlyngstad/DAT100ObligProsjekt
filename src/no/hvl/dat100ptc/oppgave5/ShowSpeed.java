fdba20f78392adf8012203223851f401e58411e4
dcj152070 <111758889+dcj152070@users.noreply.github.com>
2022-10-18 14:03:45 +0200

    Update ShowSpeed.java

--- src/no/hvl/dat100ptc/oppgave5/ShowSpeed.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowSpeed.java
@@ -11,22 +11,22 @@ import no.hvl.dat100ptc.oppgave3.GPSUtils;
 import no.hvl.dat100ptc.oppgave4.GPSComputer;
 
 public class ShowSpeed extends EasyGraphics {
-			
+
 	private static final int MARGIN = 50;
 	private static final int BARHEIGHT = 200; // assume no speed above 200 km/t
 
 	private GPSComputer gpscomputer;
 	private GPSPoint[] gpspoints;
-	
+
 	public ShowSpeed() {
 
 		String filename = JOptionPane.showInputDialog("GPS data filnavn: ");
 		gpscomputer = new GPSComputer(filename);
 
 		gpspoints = gpscomputer.getGPSPoints();
-		
+
 	}
-	
+
 	// read in the files and draw into using EasyGraphics
 	public static void main(String[] args) {
 		launch(args);
@@ -34,36 +34,35 @@ public class ShowSpeed extends EasyGraphics {
 
 	public void run() {
 
-		int N = gpspoints.length-1; // number of data points
-		
-		makeWindow("Speed profile", 2*MARGIN + 2 * N, 2 * MARGIN + BARHEIGHT);
-		
-		showSpeedProfile(MARGIN + BARHEIGHT,N);
+		int N = gpspoints.length - 1; // number of data points
+
+		makeWindow("Speed profile", 2 * MARGIN + 2 * N, 2 * MARGIN + BARHEIGHT);
+
+		showSpeedProfile(MARGIN + BARHEIGHT, N);
 	}
-	
+
 	public void showSpeedProfile(int ybase, int N) {
 
-		// get segments speeds from the GPS computer object		
+		// get segments speeds from the GPS computer object
 		double[] speeds = gpscomputer.speeds();
 
-		int x = MARGIN,y;
-		
-		setColor(0,0,255);
+		int x = MARGIN, y;
 
+		setColor(0, 0, 255);
 
 		// TODO - START
-		
+
 		for (int i = 0; i < speeds.length; i++) {
-			y = ybase - (int)speeds[i];
-			drawLine(x,y,x,ybase);
-			x+=2;
+			y = ybase - (int) speeds[i];
+			drawLine(x, y, x, ybase);
+			x += 2;
 		}
 
-		setColor(0,255,0);
+		setColor(0, 255, 0);
 
-		System.out.println("Average speed: " + (int)gpscomputer.averageSpeed());
+		System.out.println("Average speed: " + (int) gpscomputer.averageSpeed());
 
-		drawLine(MARGIN, ybase - (int)gpscomputer.averageSpeed(), x, ybase - (int)gpscomputer.averageSpeed());	
+		drawLine(MARGIN, ybase - (int) gpscomputer.averageSpeed(), x, ybase - (int) gpscomputer.averageSpeed());
 		// TODO - SLUTT
 	}
 }
